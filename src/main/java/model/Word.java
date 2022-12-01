212fb5cafbd286788844f80d54f6b4765d199c27
Leopold Abignoli <leopold.abignoli@gmail.com>
2022-12-01 16:16:08 +0100

    Word class

--- src/main/java/model/Word.java
+++ src/main/java/model/Word.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Word {
+
+    private String content;
+
+    public Word(String content) {
+        this.content = content;
+    }
+
+    /** 
+     * @return String
+     */
+    public String getContent() {
+        return content;
+    }
     
+    /** 
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
